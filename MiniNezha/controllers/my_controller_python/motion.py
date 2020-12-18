a9efa384b6523730295a169665e01e521eb25a33
Penson-BX <asqrew@126.com>
2020-12-19 00:24:05 +0800

    push new wbt

--- MiniNezha/controllers/my_controller_python/motion.py
+++ MiniNezha/controllers/my_controller_python/motion.py
@@ -351,10 +351,10 @@ class velocity_controller:
 
     def keyboardControl(self,robot,key):
         if key == 87:  # 'w' 前进
-            self.setXVel(10)
+            self.setXVel(5)
             print('forward')
         elif key == 83:  # 's' 后退
-            self.setXVel(-10)
+            self.setXVel(-5)
             print('backward')
         elif key == 65:  # 'a' 左转
             self.setAVel(key, 0.6)
